15b0efa18b889c509bf0d548fc17201098cbba55
Pavel Sinenka <sinenko.pavel72@gmail.com>
2025-12-27 13:23:34 +0300

    lesson04: выполнен до конца

--- src/by/it/group451051/sinenko/lesson04/C_GetInversions.java
+++ src/by/it/group451051/sinenko/lesson04/C_GetInversions.java
@@ -46,21 +46,51 @@ public class C_GetInversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int result = 0;
+        
         //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
 
-
-
-
-
-
-
-
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        scanner.close();
+        return (int) countInversions(a);
     }
 
+    // метод для подсчета инверсий
+    private long countInversions(int[] arr) {
+        return mergeSortAndCount(arr, new int[arr.length], 0, arr.length - 1);
+    }
+
+    // метод сортировки с подсчетом инверсий
+    private long mergeSortAndCount(int[] arr, int[] temp, int left, int right) {
+        if (left >= right) return 0;
+        long invCount = 0;
+        int mid = left + (right - left) / 2;
+
+        invCount += mergeSortAndCount(arr, temp, left, mid);
+        invCount += mergeSortAndCount(arr, temp, mid + 1, right);
+        invCount += mergeAndCount(arr, temp, left, mid, right);
+
+        return invCount;
+    }
+
+    // cлияние с подсчетом инверсий
+    private long mergeAndCount(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left, j = mid + 1, k = left;
+        long invCount = 0;
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                invCount += (mid - i + 1);
+            }
+        }
+
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+
+        System.arraycopy(temp, left, arr, left, right - left + 1);
+        return invCount;
+    }
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
